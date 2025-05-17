448140a880c9d2df3f38a4a4464e91fad76b8824
CamiloCuenca <camilocuenca1810@gmail.com>
2025-05-17 17:59:56 -0500

    se configuró CORS para permitir solicitudes desde cualquier origen

--- src/main/java/edu/uniquindio/exami/Controllers/AutenticacionController.java
+++ src/main/java/edu/uniquindio/exami/Controllers/AutenticacionController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*") // Permitir solicitudes de cualquier origen para desarrollo
 public class AutenticacionController {
 
     private final AutenticacionService autenticacionService;
